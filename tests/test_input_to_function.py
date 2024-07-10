c9f0030f1ce49bbf395cbb8c6493ed2c373df860
Kemal Bidzhiev <kemal@Kemals-MacBook-Pro.local>
2024-07-10 15:12:34 +0200

    tests for terminal input

--- tests/test_input_to_function.py
+++ tests/test_input_to_function.py
@@ -12,4 +12,17 @@ def test_function_from_input():
     assert isclose(f(np.pi/4), 1/np.sqrt(2))
     assert not isclose(f(0), 0.1)
 
+    input_str = "np.sin(x) + 0.1"
+    f = function_from_input(input_str)
+    assert isclose(f(0), 0.1)
+    assert isclose(f(np.pi/2), 1.1)
+    assert isclose(f(np.pi/4), 1/np.sqrt(2)+0.1)
+    assert not isclose(f(0), 0)
+
+    input_str = "np.sum(np.abs(x * np.sin(x) + 0.1 * x))"
+    f = function_from_input(input_str)
+    x = np.array([[1, 2], [3, 4]])
+    assert isclose(f(x), 6.310635843870575)
+
+
 
--- ttoptSDK/input_to_function.py
+++ ttoptSDK/input_to_function.py
@@ -6,12 +6,12 @@ def function_from_input(input_str):
     return f
 
 # Example terminal input string
-#terminal_input = "np.sum(np.abs(X * np.sin(X) + 0.1 * X), axis=1)"
+terminal_input = "np.sum(np.abs(x * np.sin(x) + 0.1 * x))"
 
 # Create the function
-#f = create_function_from_input(terminal_input)
+f = function_from_input(terminal_input)
 
 # Test the created function
-#X = np.array([[1, 2, 3], [4, 5, 6]])
-#result = f(X)
-#print(result)
+x = np.array([[1, 2], [3, 4]])
+result = f(x)
+print(result)
