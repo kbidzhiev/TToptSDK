dfef6ddbdab5bacb7bbe75a1ef160bb5cb089673
Kemal Bidzhiev <kemal@Kemals-MacBook-Pro.local>
2024-07-10 16:19:40 +0200

    .

--- ttoptSDK/input_to_function.py
+++ ttoptSDK/input_to_function.py
@@ -1,17 +1,4 @@
-import numpy as np
-
 def function_from_input(input_str):
     def f(x):
         return eval(input_str)
     return f
-
-# Example terminal input string
-terminal_input = "np.sum(np.abs(x * np.sin(x) + 0.1 * x))"
-
-# Create the function
-f = function_from_input(terminal_input)
-
-# Test the created function
-x = np.array([[1, 2], [3, 4]])
-result = f(x)
-print(result)
